43fcc3bc66ad3baa1df412e48620e792a07ee321
yurmchg <yurii.machuga@ralabs.org>
2019-07-29 13:30:38 +0300

    .

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 debug = True
-database = "cryptopuzzle_sandbox"
-user = "yurii"
-password = "yurii"
+database = "demo_database"
+user = "demo_user"
+password = "demo_password"
 host = "localhost"
 port = "5432"
 SQLALCHEMY_DATABASE_URI = f'postgresql://{user}:{password}@{host}/{database}'
